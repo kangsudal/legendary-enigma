0f7ccb5f13e458ed8bdea4c5baabeb705b13feb9
kangsudal <kangsudal@gmail.com>
2019-01-15 18:05:46 +0900

    ãMake clear url pattern.

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('', views.home, name='home'),
+    path('post/list/', views.post_list, name='post_list'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('tool/', views.tool, name='tool'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -49,3 +49,7 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def tool(request):
+    return render(request, 'blog/tool.html',{})    
