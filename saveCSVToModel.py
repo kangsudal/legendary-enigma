27f9b62fe2994ed8874bec6b17029f5f397f6876
kangsudal <kangsudal@gmail.com>
2019-03-18 14:52:03 +0900

    change database_url.

--- saveCSVToModel.py
+++ saveCSVToModel.py
@@ -26,14 +26,18 @@ VERSION = '9.2.1'
 user = settings.DATABASES['default']['USER']
 password = settings.DATABASES['default']['PASSWORD']
 database_name = settings.DATABASES['default']['NAME']
+host = settings.DATABASES['default']['HOST']
+port = 5432
 
-#print(user,password,database_name)
+print(user,password,database_name,host,port)
 
 #postgresql에 연결
-database_url = 'postgresql://{user}:{password}@localhost:5432/{database_name}'.format(
+database_url = 'postgresql://{user}:{password}@{host}:{port}/{database_name}'.format(
     user=user,
     password=password,
     database_name=database_name,
+    host=host,
+    port=port,
 )
 engine = create_engine(database_url, echo=False)
 
